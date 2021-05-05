b0bda7508f9c226f75d00265e7c974c5ed1afa07
NoisyPillow <ycazee@gmail.com>
2021-05-05 19:51:17 +0200

    Updated SYMBOL and ASSET usage

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from requests.models import ProtocolError
 from utilities import TweetListener
 
 ASSET = ["USDT", 11]
-SYMBOL = "DOGEUSDT"
+SYMBOL = "DOGE"
 
 f = open("keys.txt", 'r')
 keys = json.loads(f.read())
@@ -27,42 +27,50 @@ URL = LIVE_URL
 SECRET_KEY = LIVE_SECRET_KEY
 PUB_KEY = LIVE_PUB_KEY
 
+
 def get_precision():
     exchange_info = requests.get(URL + "exchangeInfo")
     exchangeInfo = exchange_info.json()
     for crypto in exchangeInfo['symbols']:
-        if crypto['symbol'] == SYMBOL:
+        if crypto['symbol'] == SYMBOL + ASSET[0]:
             for filters in crypto['filters']:
                 if filters['filterType'] == 'LOT_SIZE':
-                        step_size = float(filters['stepSize'])
+                    step_size = float(filters['stepSize'])
     precision = 0
     while step_size < 1:
         step_size *= 10
         precision += 1
     return precision
 
+
 PRECISION = get_precision()
+
 
 def get_timestamp():
     time_request = requests.get(URL + "time")
     timestamp = time_request.json()['serverTime']
     return timestamp
 
+
 def round_down(n, decimals=0):
-    multiplier = 10 ** decimals
+    multiplier = 10**decimals
     return math.floor(n * multiplier) / multiplier
 
+
 def get_quantity():
-    symbol_info = requests.get(URL + f"ticker/price?symbol={SYMBOL}")
+    symbol_info = requests.get(URL +
+                               f"ticker/price?symbol={SYMBOL + ASSET[0]}")
     price = symbol_info.json()['price']
-    quantity = ASSET[1]/float(price)
+    quantity = ASSET[1] / float(price)
     quantity = float(round(quantity, PRECISION))
     return quantity
+
 
 def get_asset_balance():
     TOTAL_PARAMS = f"timestamp={get_timestamp()}"
     TOTAL_PARAMS_b = TOTAL_PARAMS.encode('ASCII')
-    signature = hmac.new(SECRET_KEY, TOTAL_PARAMS_b, hashlib.sha256).hexdigest()
+    signature = hmac.new(SECRET_KEY, TOTAL_PARAMS_b,
+                         hashlib.sha256).hexdigest()
     payload = TOTAL_PARAMS + "&signature=" + signature
     headers = {'X-MBX-APIKEY': PUB_KEY}
 
@@ -71,6 +79,7 @@ def get_asset_balance():
     for balance in balances:
         if balance['asset'] == ASSET[0]:
             return float(balance['free'])
+
 
 def get_details(response):
     sides = {'SELL': "Sold", 'BUY': "Bought"}
@@ -85,25 +94,27 @@ def get_details(response):
         orders += 1
     return str_to_print, commission
 
+
 def get_order_status(order_id):
-    TOTAL_PARAMS = f"symbol={SYMBOL}&orderId={order_id}&timestamp={get_timestamp()}"
+    TOTAL_PARAMS = f"symbol={SYMBOL + ASSET[0]}&orderId={order_id}&timestamp={get_timestamp()}"
     TOTAL_PARAMS_b = TOTAL_PARAMS.encode('ASCII')
-    signature = hmac.new(SECRET_KEY, TOTAL_PARAMS_b, hashlib.sha256).hexdigest()
+    signature = hmac.new(SECRET_KEY, TOTAL_PARAMS_b,
+                         hashlib.sha256).hexdigest()
     payload = TOTAL_PARAMS + "&signature=" + signature
     headers = {'X-MBX-APIKEY': PUB_KEY}
-    
+
     order_info = requests.get(URL + "order?" + payload, headers=headers)
     response = order_info.json()
     return response
 
 
-
 def buy(retry):
     quantity = get_quantity()
 
-    TOTAL_PARAMS = f"symbol={SYMBOL}&side=BUY&type=MARKET&quantity={quantity}&recvWindow=3000&timestamp={get_timestamp()}"
+    TOTAL_PARAMS = f"symbol={SYMBOL + ASSET[0]}&side=BUY&type=MARKET&quantity={quantity}&recvWindow=3000&timestamp={get_timestamp()}"
     TOTAL_PARAMS_b = TOTAL_PARAMS.encode('ASCII')
-    signature = hmac.new(SECRET_KEY, TOTAL_PARAMS_b, hashlib.sha256).hexdigest()
+    signature = hmac.new(SECRET_KEY, TOTAL_PARAMS_b,
+                         hashlib.sha256).hexdigest()
     payload = TOTAL_PARAMS + "&signature=" + signature
     headers = {'X-MBX-APIKEY': PUB_KEY}
 
@@ -111,10 +122,12 @@ def buy(retry):
     response = order_created.json()
     str_to_print, commission = get_details(response)
     if response['status'] == 'FILLED':
-        print(datetime.now().strftime("\033[92m%d-%m-%Y %H:%M:%S | \033[0m") + f"\033[92m{str_to_print}\033[0m")
+        print(datetime.now().strftime("\033[92m%d-%m-%Y %H:%M:%S | \033[0m") +
+              f"\033[92m{str_to_print}\033[0m")
         return response['orderId'], commission
     elif retry > 0:
-        print(datetime.now().strftime("\033[91m%d-%m-%Y %H:%M:%S | \033[0m") + "\033[91mOrder error\033[0m")
+        print(datetime.now().strftime("\033[91m%d-%m-%Y %H:%M:%S | \033[0m") +
+              "\033[91mOrder error\033[0m")
         print(r.content.decode())
         print("\033[91mRetrying...\033[0m")
         retry -= 1
@@ -128,10 +141,11 @@ def sell(order_id, retry, commission):
     response = get_order_status(order_id)
     quantity = float(response['executedQty']) - commission
     quantity = round_down(quantity, PRECISION)
-    
-    TOTAL_PARAMS = f"symbol={SYMBOL}&side=SELL&type=MARKET&quantity={quantity}&recvWindow=4000&timestamp={get_timestamp()}"
+
+    TOTAL_PARAMS = f"symbol={SYMBOL + ASSET[0]}&side=SELL&type=MARKET&quantity={quantity}&recvWindow=4000&timestamp={get_timestamp()}"
     TOTAL_PARAMS_b = TOTAL_PARAMS.encode('ASCII')
-    signature = hmac.new(SECRET_KEY, TOTAL_PARAMS_b, hashlib.sha256).hexdigest()
+    signature = hmac.new(SECRET_KEY, TOTAL_PARAMS_b,
+                         hashlib.sha256).hexdigest()
     payload = TOTAL_PARAMS + "&signature=" + signature
     headers = {'X-MBX-APIKEY': PUB_KEY}
 
@@ -139,9 +153,11 @@ def sell(order_id, retry, commission):
     response = order_created.json()
     str_to_print, commission = get_details(response)
     if response['status'] == 'FILLED':
-        print(datetime.now().strftime("\033[94m%d-%m-%Y %H:%M:%S | \033[0m") + f"\033[94m{str_to_print}\033[0m")
+        print(datetime.now().strftime("\033[94m%d-%m-%Y %H:%M:%S | \033[0m") +
+              f"\033[94m{str_to_print}\033[0m")
     elif retry > 0:
-        print(datetime.now().strftime("\033[91m%d-%m-%Y %H:%M:%S | \033[0m") + "\033[91mOrder error\033[0m")
+        print(datetime.now().strftime("\033[91m%d-%m-%Y %H:%M:%S | \033[0m") +
+              "\033[91mOrder error\033[0m")
         print(r.content.decode())
         print("\033[91mRetrying...\033[0m")
         retry -= 1
@@ -150,9 +166,11 @@ def sell(order_id, retry, commission):
         print("guess we'll pass for this time...")
         retry = 0
 
+
 def got_tweet(status):
     if get_asset_balance() > ASSET[1]:
-        print(datetime.now().strftime(f"%d-%m-%Y %H:%M:%S | ") + "Elon just tweeted about $DOGE:")
+        print(datetime.now().strftime(f"%d-%m-%Y %H:%M:%S | ") +
+              f"Elon just tweeted about ${SYMBOL}:")
         print("    " + status.text)
         order_id, commission = buy(3)
         sleep(5)
@@ -160,6 +178,7 @@ def got_tweet(status):
         print("----------------------------------------------------------")
     else:
         print("Not enough liquidity.")
+
 
 if __name__ == "__main__":
     while True:
@@ -172,4 +191,3 @@ if __name__ == "__main__":
         except ProtocolError:
             print("ProtocolError, retying..")
             pass
-
