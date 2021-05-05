792e77d1e9b1b19bdd5098c2eb26fed508142ea6
NoisyPillow <ycazee@gmail.com>
2021-05-05 19:26:09 +0200

    first commit

---
+++ utilities.py
@@ -0,0 +1,45 @@
+from types import FunctionType
+import tweepy
+
+def verify(status, keywords):
+    if hasattr(status, 'retweeted_status'):
+        return False
+    if status.in_reply_to_status_id != None:
+        return False
+    elif status.in_reply_to_screen_name != None:
+        return False
+    elif status.in_reply_to_user_id != None:
+        return False
+    elif keywords in status.text.lower():
+        return True
+    else:
+        print("False alert.")
+
+class TweetListener(tweepy.StreamListener):
+    def __init__(self, tweeter_id: str, keywords: str, exec_func):
+        super().__init__() #inits mother class
+        self.tweeter_id = tweeter_id #id of the tracked profile
+        self.keywords = keywords #keywords wich have to be in the tweet
+        self.exec_func = exec_func #function to be executed on verifed status
+
+        auth = tweepy.OAuthHandler("VBc6RgtwsW7SzBVPAHW0D8q9w", "PDVvgra3rPsQbrfxG3tpHhe9qB2jlwaUFNnshBk2rkC0bqEkaJ")
+        auth.set_access_token("884457459183738880-v73XQPDNV54iqycRi5XanfNqv2UzRPo", "7ugYRiUOb9HmA7WUP1uIDqJ9hGRyxzk6Op0URn1NwEZie")
+
+        self.stream = tweepy.Stream(auth = auth, listener = self)
+        self.update()
+
+    def update(self):
+        self.stream.filter(follow=[self.tweeter_id])
+
+    def set_tweeter_id(self, tweeter_id):
+        self.tweeter_id = tweeter_id
+        self.update()
+
+    def set_keywords(self, keywords):
+        self.keywords = keywords
+        self.update()
+
+    def on_status(self, status):
+        if verify(status, self.keywords):
+            self.exec_func(status)
+
